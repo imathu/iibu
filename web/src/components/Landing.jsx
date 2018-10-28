056c3059639b9b93392b9e2c175d63ef85d5b7d5
Mathias Herzog <mathu@Mathiass-MBP.fritz.box>
2018-10-28 17:06:01 +0100

    refactor landing page

--- web/src/components/Landing.jsx
+++ web/src/components/Landing.jsx
@@ -7,43 +7,81 @@ import AuthUserContext from 'components/AuthUserContext';
 
 const LandingPage = ({ mobile }) => (
   <AuthUserContext.Consumer>
-    {auth => (
-      <div>
-        <Container text>
+    {auth => (!auth
+      ?
+        <div>
+          <Container text textAlign="center">
+            <Header
+              as="h1"
+              style={{
+                fontSize: mobile ? '2em' : '4em',
+                fontWeight: 'normal',
+                marginBottom: 0,
+                marginTop: mobile ? '1em' : '1em',
+                textAlign: 'center',
+              }}
+            >
+              <FormattedMessage
+                id="app.title"
+                defaultMessage="realfeedback.ch"
+                values={{ what: 'react-intl' }}
+              />
+            </Header>
+            <Header
+              as="h2"
+              style={{
+                fontSize: mobile ? '1.5em' : '1.7em',
+                fontWeight: 'normal',
+                marginTop: mobile ? '0.1em' : '1em',
+                textAlign: 'center',
+              }}
+            >
+              <FormattedMessage
+                id="app.intro"
+                defaultMessage="professionelle 360 Grad Feedbackanalyse für Führungskräfte"
+                values={{ what: 'react-intl' }}
+              />
+            </Header>
+            <FormattedMessage
+              id="app.contact"
+              defaultMessage="kontaktiere info at realfeedback.ch"
+              values={{ what: 'react-intl' }}
+            />
+          </Container>
+        </div>
+        :
+        <Container
+          style={{
+            width: mobile ? '80%' : '40%',
+          }}
+          text
+          textAlign="center"
+        >
           <Header
             as="h1"
-            style={{
-              fontSize: mobile ? '2em' : '4em',
-              fontWeight: 'normal',
-              marginBottom: 0,
-              marginTop: mobile ? '1.5em' : '3em',
-              textAlign: 'center',
-            }}
+            style={{ marginTop: mobile ? '0.1em' : '3em' }}
           >
             <FormattedMessage
-              id="app.title"
-              defaultMessage="Willkommen bei realfeedback.ch"
+              id="app.loginMessage"
+              defaultMessage="Willkommen"
+              values={{ what: 'react-intl' }}
+            />
+            <i> {auth.authUser.email} </i>
+          </Header>
+          <Header as="h2">
+            <FormattedMessage
+              id="app.feedbackerThanks"
+              defaultMessage="Danke dass du bei realfeedback.ch mitmachst"
               values={{ what: 'react-intl' }}
             />
           </Header>
-          <Header
-            as="h2"
-            style={{
-              fontSize: mobile ? '1.5em' : '1.7em',
-              fontWeight: 'normal',
-              marginTop: mobile ? '0.5em' : '1.5em',
-              textAlign: 'center',
-            }}
-          >
-            <FormattedMessage
-              id="app.intro"
-              defaultMessage="360 Grad Feedbackanalyse für Führungskräfte"
-              values={{ what: 'react-intl' }}
-            />
-          </Header>
-
+          <br /><hr /><br />
+          <FormattedMessage
+            id="app.feedbackerMessage"
+            defaultMessage="Für die Teilnahme an einer Feedbackrunde erhälst du von deinem Organisator einen Link per Mail"
+            values={{ what: 'react-intl' }}
+          />
         </Container>
-      </div>
     )}
   </AuthUserContext.Consumer>
 );
