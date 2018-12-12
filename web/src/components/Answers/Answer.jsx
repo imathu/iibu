4e499c92e31309948ee8b5c5a5c34f20aa28cd0d
Mathias Herzog <mathu@Mathiass-MBP.fritz.box>
2018-12-12 21:36:23 +0100

    revert last changes

--- web/src/components/Answers/Answer.jsx
+++ web/src/components/Answers/Answer.jsx
@@ -29,7 +29,7 @@ class Answer extends React.Component {
       feedbacker.clients[clientId].answers &&
       feedbacker.clients[clientId].answers[questionId] &&
       feedbacker.clients[clientId].answers[questionId].score);
-    this.setState(() => ({ value: parseInt(value, 10) }));
+    this.setState(() => ({ value }));
   }
   handleChange = (e, { value }) => {
     this.setState(() => ({ value }));
@@ -63,7 +63,7 @@ class Answer extends React.Component {
                   radio
                   name="checkboxRadioGroup"
                   value={i}
-                  checked={value === parseInt(i)}
+                  checked={value === i}
                   onChange={this.handleChange}
                 />
               </Form.Field>
